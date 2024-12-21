6d7bce962093ac975f481d0579022eee2115eb76
thegoodone14 <112936691+thegoodone14@users.noreply.github.com>
2024-12-21 21:17:34 +0100

    Update taskRoutes.js

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,14 +1,13 @@
-
 import express from 'express';
 import { getTasksByListId, addTaskToUniqueList, toggleTaskStatus, deleteTask } from '../controllers/taskController.js';
 import { authenticateToken } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
-// Route pour récupérer les tâches d'une liste spécifique
-router.get('/:ID_List/tasks', authenticateToken, getTasksByListId);
-router.post("/tasks", authenticateToken, addTaskToUniqueList);
-router.put('/:Id_Task', authenticateToken, toggleTaskStatus);
-router.delete('/tasks/:ID_Task', authenticateToken, deleteTask)
+// Routes avec chemins standardisés
+router.get('/list/:ID_List/tasks', authenticateToken, getTasksByListId);
+router.post('/tasks', authenticateToken, addTaskToUniqueList);
+router.put('/tasks/:Id_Task/status', authenticateToken, toggleTaskStatus);
+router.delete('/tasks/:ID_Task', authenticateToken, deleteTask);
 
 export default router;
