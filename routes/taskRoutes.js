1ea3e244b6e01270ab5e30443e6d551bf2ab57c3
thegoodone14 <112936691+thegoodone14@users.noreply.github.com>
2024-12-21 22:08:46 +0100

    Fix: Double "list"in URL

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -5,7 +5,7 @@ import { authenticateToken } from '../middleware/authMiddleware.js';
 const router = express.Router();
 
 // Routes avec chemins standardisés
-router.get('/list/:ID_List/tasks', authenticateToken, getTasksByListId);
+router.get('/:ID_List/tasks', authenticateToken, getTasksByListId);  // Enlevé le préfixe /list
 router.post('/tasks', authenticateToken, addTaskToUniqueList);
 router.put('/tasks/:Id_Task/status', authenticateToken, toggleTaskStatus);
 router.delete('/tasks/:ID_Task', authenticateToken, deleteTask);
