9261bf06f1c1b15d768451dc9abd742c1a0b3c24
thegoodone14 <112936691+thegoodone14@users.noreply.github.com>
2024-12-22 00:07:48 +0100

    Fix: Allow frontend (CORS)

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://todo-frontend-bay-nu.vercel.app/'],
+  origin: ['http://localhost:3000', 'https://todo-frontend-bay-nu.vercel.app'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
@@ -22,7 +22,7 @@ app.use(cors({
 
 // Middleware pour ajouter les headers CORS à chaque réponse
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Origin', 'https://todo-frontend-bay-nu.vercel.app');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.header('Access-Control-Allow-Credentials', 'true');
